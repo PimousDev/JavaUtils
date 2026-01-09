16842c2ab23d3e3a0f9afccb468a9efbfa0c5754
Xibitol <xibitol@pimous.dev>
2026-01-09 17:30:44 +0100

    Added placeholder files for pucutils subproject.

---
+++ pucutils/src/pu/cutils/pucutils.h
@@ -0,0 +1,22 @@
+/* Project Utils (Build tools and Utility libraries/classes)
+Copyright © 2025 - Pimous Dev. (https://www.pimous.dev/)
+
+These programs are free software: you can redistribute them and/or modify them
+under the terms of the GNU Lesser General Public License version 3 as published
+by the Free Software Foundation.
+
+The latters are distributed in the hope that they will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License and the GNU
+Lesser General Public License along with the programs (Links: COPYING &
+COPYING.LESSER). If not, see https://www.gnu.org/licenses/.
+*/
+
+#pragma once
+
+#define _PUCUTILS_INSIDE
+
+#undef _PUCUTILS_INSIDE
